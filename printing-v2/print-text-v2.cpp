9b13c5ea48b5f68b37c1e4d56758bf1d0709b930
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-04 18:08:26 +0100

    commit print-text

---
+++ printing-v2/print-text-v2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I love programming!\n\n";
+    cout << "I promise to be the best developer ever!\n\n";
+    cout << "I know it will take sometimes to practice, but I will acheive my goal.\n\n";
+    cout << "Best regards,\n";
+    cout << "Khadija\n";
+    return 0;
+}
