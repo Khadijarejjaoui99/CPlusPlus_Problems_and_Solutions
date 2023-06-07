154d8b32719e4745d32975551653e2d889f72217
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-07 18:40:07 +0100

    commit data-type-size.cpp

---
+++ size/data-type-size.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Size of int data type is: " << sizeof(int) << endl;
+    cout << "Size of short data type is: " << sizeof(short) << endl;
+    cout << "Size of long data type is: " << sizeof(long) << endl;
+    cout << "Size of long long data type is: " << sizeof(long long) << endl;
+    cout << "Size of float data type is: " << sizeof(float) << endl;
+    cout << "Size of double data type is: " << sizeof(double) << endl;
+    cout << "Size of char data type is: " << sizeof(char) << endl;
+    cout << "Size of string data type is: " << sizeof(string) << endl;
+    cout << "Size of bool data type is: " << sizeof(bool) << endl;
+    return 0;
+}
