2a5f20f679b1cccb48203987f3e1a16736b85b80
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-07 07:42:52 +0100

    commit read-age-calculation

---
+++ reading/read-age-calculation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Please enter your age:\n";
+    cin >> age;
+
+    cout << "After 5 years you will be " << age + 5 << " years old.\n";
+}
