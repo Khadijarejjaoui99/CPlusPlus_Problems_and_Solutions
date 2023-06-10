ff3a958e32da1296a1a608eb8695118ae7fc12e2
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-10 23:34:34 +0100

    update data-type-conversion

--- CPlusPlus-Homeworks/data-type-conversion/data-type-conversion-homework.cpp
+++ CPlusPlus-Homeworks/data-type-conversion/data-type-conversion-homework.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     // 1- Convert string St1 = "43.22" to float, double, integer.
+
     string St1 = "43.22";
     float stFloat = stof(St1);
     double stDouble = stod(St1);
@@ -14,6 +15,11 @@ int main()
     cout << "type of stFloat is: " << typeid(stFloat).name() << endl;
     cout << "type of stDouble is: " << typeid(stDouble).name() << endl;
     cout << "type of stInt is: " << typeid(stInt).name() << endl;
+
+    // 2- Convert integer N1 = 20 to string.
+
+    int N1 = 20;
+    string Str1 = to_string(N1);
 
     return 0;
 }
