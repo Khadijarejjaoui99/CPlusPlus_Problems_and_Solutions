3e30aa2181ddb57e391d547f236aa1ff9edc85cd
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-11 11:28:47 +0100

    update data-type-conversion

---
+++ CPlusPlus-Homeworks/data-type-conversion/data-type-conversion-homework5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float Num;
+    char C;
+
+    cout << "please enter a decimal number:\n";
+    cin >> Num;
+
+    C = char(Num);
+
+    cout << C << endl;
+
+    return 0;
+}
