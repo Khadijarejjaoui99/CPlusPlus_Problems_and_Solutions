df9bce99a65479d37e706bfd936c17317d3cc0fd
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-12 20:51:09 +0100

    update functions

--- CPlusPlus-Homeworks/functions/procedure-and-function.cpp
+++ CPlusPlus-Homeworks/functions/procedure-and-function.cpp
@@ -13,7 +13,20 @@ void MySumProcedure()
     cout << Number1 + Number2 << endl;
 }
 
+int MySumFunction()
+{
+    int Number1, Number2;
+    cout << "Please enter Number1:\n";
+    cin >> Number1;
+    cout << "Please enter Number2:\n";
+    cin >> Number2;
+
+    cout << "**********************\n";
+    return Number1 + Number2;
+}
 int main()
 {
     MySumProcedure();
+    cout << MySumFunction() << endl;
+    return 0;
 }
