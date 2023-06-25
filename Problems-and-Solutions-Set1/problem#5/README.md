150194bbb83dea753a9b4249e2eaa4533ef49992
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-25 21:05:02 +0100

    hire-a-driver-2

---
+++ Problems-and-Solutions-Set1/problem#5/README.md
@@ -0,0 +1,9 @@
+## Problem #5: Hire a driver case II
+
+Write a program that ask the user to enter his/her:
+<br> - Age
+<br> - Has driving license.
+<br> - Has recommendation
+<br> Write a program that ask the user to enter his/her:
+Then print "Hired" if s/he has recommendations or if his/her age is greater than
+21 years old and she/he has a driving license, otherwise print "Rejected".
---
+++ Problems-and-Solutions-Set1/problem#5/hire-a-driver-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct stInfo
+{
+    short Age;
+    bool HasDrivingLicence;
+    bool HasRecommendation;
+};
+
+stInfo ReadInfo()
+{
+    stInfo Info;
+
+    cout << "Please enter your age:\n";
+    cin >> Info.Age;
+    cout << "Do you have a driving licence:\n";
+    cin >> Info.HasDrivingLicence;
+    cout << "Do you have a recommendation:\n";
+    cin >> Info.HasRecommendation;
+    return Info;
+}
+
+bool IsAccepted(stInfo Info)
+{
+    return (Info.HasRecommendation || (Info.Age > 21 && Info.HasDrivingLicence));
+}
+
+void PrintResult(stInfo Info)
+{
+    if (IsAccepted(Info))
+        cout << "Hired\n";
+    else
+        cout << "Rejected\n";
+}
+
+int main()
+{
+    PrintResult(ReadInfo());
+
+    return 0;
+}
