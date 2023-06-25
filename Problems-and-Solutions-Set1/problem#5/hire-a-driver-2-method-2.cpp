9b7671c8f50b16fb0a86e66336ba2f56827b54ee
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-25 21:22:18 +0100

    hire-a-driver-2-method-2

---
+++ Problems-and-Solutions-Set1/problem#5/hire-a-driver-2-method-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct stInfo
+{
+    short Age;
+    bool HasDrivingLicence;
+    bool HasRecommendation;
+};
+
+stInfo ReadInfo()
+{
+    stInfo Info;
+
+    cout << "Please enter your age:\n";
+    cin >> Info.Age;
+    cout << "Do you have a driving licence?\n";
+    cin >> Info.HasDrivingLicence;
+    cout << "Do you have a recommendation?\n";
+    cin >> Info.HasRecommendation;
+    return Info;
+}
+
+bool IsAccepted(stInfo Info)
+{
+    if (Info.HasRecommendation)
+        return true;
+    else
+        return (Info.Age > 21 && Info.HasDrivingLicence);
+}
+
+void PrintResult(stInfo Info)
+{
+    if (IsAccepted(Info))
+        cout << "Hired\n";
+    else
+        cout << "Rejected\n";
+}
+
+int main()
+{
+    PrintResult(ReadInfo());
+
+    return 0;
+}
