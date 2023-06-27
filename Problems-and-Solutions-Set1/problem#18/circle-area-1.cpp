c1f79d28c8d501e811a7ba03286b82221c067d75
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-27 22:24:44 +0100

    circle-area

---
+++ Problems-and-Solutions-Set1/problem#18/circle-area-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float ReadRadius()
+{
+    float R;
+
+    cout << "Please enter Radius R:" << endl;
+    cin >> R;
+
+    return R;
+}
+
+float CalculateCircleArea(float R)
+{
+    const float PI = 3.14;
+
+    return PI * pow(R, 2);
+}
+
+void PrintArea(float Area)
+{
+    cout << "\nArea = " << Area << endl;
+}
+
+int main()
+{
+    PrintArea(CalculateCircleArea(ReadRadius()));
+
+    return 0;
+}
