473b923ba6df7a620daed71ad0f1b3c3925b8ba2
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-06-28 22:43:07 +0100

    circle-area

---
+++ Problems-and-Solutions-Set1/problem#19/circle-area-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float ReadDiameter()
+{
+    float D;
+
+    cout << "Please enter Diameter D:" << endl;
+    cin >> D;
+
+    return D;
+}
+
+float CircleAreaThroughDiameter(float D)
+{
+    const float PI = 3.14;
+
+    return (PI * pow(D, 2)) / 4;
+}
+
+void PrintArea(float Area)
+{
+    cout << "\nArea = " << Area << endl;
+}
+
+int main()
+{
+    PrintArea(CircleAreaThroughDiameter(ReadDiameter()));
+
+    return 0;
+}
