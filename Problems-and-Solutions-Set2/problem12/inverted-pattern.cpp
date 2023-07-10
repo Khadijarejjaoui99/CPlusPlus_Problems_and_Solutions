b38fe7cae39b2864112d0334150da0bec6ecf76d
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-10 23:37:14 +0100

    Inverted pattern

---
+++ Problems-and-Solutions-Set2/problem12/inverted-pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber()
+{
+    int Number;
+
+    do
+    {
+        cout << "Please enter a positive number:\n";
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+void PrintInvertedPattern(int Number)
+{
+    for (int i = Number; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintInvertedPattern(ReadPositiveNumber());
+    return 0;
+}
