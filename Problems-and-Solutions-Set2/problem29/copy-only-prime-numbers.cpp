71e9a11507081ea15b38d6fa5d790b3feb293342
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-14 20:56:12 +0100

    Copy Only Prime Elements

--- Problems-and-Solutions-Set2/problem29/copy-only-prime-numbers.cpp
+++ Problems-and-Solutions-Set2/problem29/copy-only-prime-numbers.cpp
@@ -40,6 +40,9 @@ void FillArrayWithRandomNumbers(int Array[100], int &Length)
 
 enPrimeNotPrime CheckPrime(int Number)
 {
+    if (Number == 0 || Number == 1)
+        return enPrimeNotPrime::NotPrime;
+
     int M = round(Number / 2);
 
     for (int i = 2; i <= M; i++)
