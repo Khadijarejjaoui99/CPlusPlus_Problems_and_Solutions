e8a45fc5a10366b4df1c49501343fd144930f4d3
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-15 16:34:08 +0100

    negative count

---
+++ Problems-and-Solutions-Set2/problem45/count-negative-numbers.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int ReadNumberInRange(int From, int To, string Msg)
+{
+    int Number;
+
+    do
+    {
+        cout << Msg << endl;
+        cin >> Number;
+    } while (Number < From || Number > To);
+
+    return Number;
+}
+
+int GetRandomNumbers(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int Array[100], int &Length)
+{
+    Length = ReadNumberInRange(1, 100, "How many numbers do you want to enter (1 to 100):");
+
+    for (int i = 0; i < Length; i++)
+    {
+        Array[i] = GetRandomNumbers(-100, 100);
+    }
+}
+
+void PrintArray(int Array[100], int Length)
+{
+    for (int i = 0; i < Length; i++)
+        cout << Array[i] << " ";
+
+    cout << endl;
+}
+
+int CountNegativeNumbersInArray(int Array[100], int Length)
+{
+    int Counter = 0;
+
+    for (int i = 0; i < Length; i++)
+    {
+        if (Array[i] < 0)
+            Counter++;
+    }
+
+    return Counter;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Array[100], Length = 0;
+
+    FillArrayWithRandomNumbers(Array, Length);
+
+    cout << "Array Elements are: ";
+    PrintArray(Array, Length);
+
+    cout << "Negative Numbers Count is: " << CountNegativeNumbersInArray(Array, Length) << endl;
+
+    return 0;
+}
