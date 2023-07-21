83ade5f196382fc653041f1f77a09cc060dd64b2
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-21 20:12:51 +0100

    average-array-numbers

--- CPlusPlus-Homeworks-2/Create-own-library2/sum-two-arrays.cpp
+++ CPlusPlus-Homeworks-2/Create-own-library2/sum-two-arrays.cpp
@@ -7,8 +7,8 @@ int main()
 {
     int Array1[100], Array2[100], ArraySum[100], Length1 = 0, Length2 = 0;
 
-    FillArrayWithRandomNumbers(Array1, Length1);
-    FillArrayWithRandomNumbers(Array2, Length2);
+    FillArrayWithRandomNumbers(Array1, Length);
+    FillArrayWithRandomNumbers(Array2, Length);
 
     return 0;
 }
