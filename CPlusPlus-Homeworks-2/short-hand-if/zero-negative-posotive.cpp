2131596cb2103ebde814a30bcc38739793c2732c
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-22 17:36:21 +0100

    negative or positive or zero

---
+++ CPlusPlus-Homeworks-2/short-hand-if/zero-negative-posotive.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumber()
+{
+    int Number;
+
+    cout << "Please enter a number:\n";
+    cin >> Number;
+
+    return Number;
+}
+
+string NumberType(int Number)
+{
+    return (Number == 0) ? "Zero" : (Number > 0) ? "Positive"
+                                                 : "Negative";
+}
+
+int main()
+{
+    cout << NumberType(ReadNumber()) << endl;
+
+    return 0;
+}
