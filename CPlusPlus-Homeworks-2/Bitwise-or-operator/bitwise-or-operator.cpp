f0da977c808425a57f3399ab395c00e6ff2c1c6d
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-22 20:33:48 +0100

    Bitwise or operator

---
+++ CPlusPlus-Homeworks-2/Bitwise-or-operator/bitwise-or-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Result: " << (12 | 25) << endl;
+    cout << "Result: " << (200 | 125) << endl;
+    cout << "Result: " << (7 | 10) << endl;
+    cout << "Result: " << (100 | 5) << endl;
+
+    return 0;
+}
