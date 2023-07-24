4737b7ea1b40cb3ed4381dc8c2223047e8410323
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-24 16:13:00 +0100

    string format

---
+++ CPlusPlus-Homeworks-2/Printf/string-and-char-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char Name[] = "Khadija Rejjaoui";
+    char WebSite[] = "Github.com";
+
+    printf("Hello, %s, how are you?\n", Name);
+    printf("Welcome to %s\n", WebSite);
+
+    return 0;
+}
