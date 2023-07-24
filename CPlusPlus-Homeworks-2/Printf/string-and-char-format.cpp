387bd750208643152d27bb775e027301a5366594
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-24 16:26:52 +0100

    string char format

--- CPlusPlus-Homeworks-2/Printf/string-and-char-format.cpp
+++ CPlusPlus-Homeworks-2/Printf/string-and-char-format.cpp
@@ -9,5 +9,16 @@ int main()
     printf("Hello, %s, how are you?\n", Name);
     printf("Welcome to %s\n", WebSite);
 
+    char c = 'S';
+
+    printf("Character is %c\n", c);
+
+    // set width:
+
+    printf("%*c\n", 1, c);
+    printf("%*c\n", 2, c);
+    printf("%*c\n", 3, c);
+    printf("%4c\n", c);
+
     return 0;
 }
