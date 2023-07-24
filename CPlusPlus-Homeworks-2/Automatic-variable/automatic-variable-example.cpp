8cd56a475ba812e6e8b5c863e3a1f5cfdd3bf176
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-24 14:45:41 +0100

    Automatic variable

---
+++ CPlusPlus-Homeworks-2/Automatic-variable/automatic-variable-example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    auto x = 19;
+    auto y = 10.2;
+    auto z = "Hello";
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+
+    return 0;
+}
