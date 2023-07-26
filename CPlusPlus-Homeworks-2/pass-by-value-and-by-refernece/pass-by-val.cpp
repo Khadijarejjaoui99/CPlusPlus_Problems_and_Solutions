177f76dd78f48bad753e08cfa0bcb4b73be91ae6
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-07-26 20:44:29 +0100

    pass by val

---
+++ CPlusPlus-Homeworks-2/pass-by-value-and-by-refernece/pass-by-val.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void Function(int x)
+{
+    x++;
+}
+
+int main()
+{
+    int a = 10;
+
+    cout << " a value before passing it to Function(): " << a << endl;
+
+    Function(a);
+
+    cout << " a value after passing it to Function(): " << a << endl;
+
+    return 0;
+}
