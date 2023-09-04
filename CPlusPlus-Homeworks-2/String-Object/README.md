9c95c6ab91865bde22a49b2db1dce8230391df0c
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-09-04 21:15:01 +0100

    string length

---
+++ CPlusPlus-Homeworks-2/String-Object/README.md
@@ -0,0 +1,6 @@
+## Find String Length:
+
+Find the length of the following strings:
+<br> S1 = "Hello World"
+<br> S2 = "My Name is Khadija"
+<br> S3 = "How are you doing?"
---
+++ CPlusPlus-Homeworks-2/String-Object/find-string-length.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S1 = "Hello World", S2 = "My Name is Khadija", S3 = "How are you doing?";
+
+    cout << "S1 length is: " << S1.length() << endl;
+    cout << "S2 length is: " << S2.length() << endl;
+    cout << "S3 length is: " << S3.length() << endl;
+
+    return 0;
+}
