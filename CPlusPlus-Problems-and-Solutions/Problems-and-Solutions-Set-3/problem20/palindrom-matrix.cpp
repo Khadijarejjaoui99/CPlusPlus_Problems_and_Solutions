ad9618137a6254b1dfbd35102a5fabe5c2335445
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-09-08 11:53:55 +0100

    Palindrom matrix

---
+++ CPlusPlus-Problems-and-Solutions/Problems-and-Solutions-Set-3/problem20/palindrom-matrix.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void PrintMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << Matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+bool IsPalindromMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols / 2; j++)
+        {
+            if (Matrix[i][j] != Matrix[i][Cols - 1 - j])
+                return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+
+    int Matrix[3][3] = {{1, 2, 1}, {5, 5, 5}, {7, 3, 7}};
+
+    cout << "\nMatrix: \n";
+    PrintMatrix(Matrix, 3, 3);
+
+    if (IsPalindromMatrix(Matrix, 3, 3))
+        cout << "Yes: is a Palindrom matrix\n";
+    else
+        cout << "No: is Not a Palindrom matrix\n";
+
+    system("pause>0");
+
+    return 0;
+}
