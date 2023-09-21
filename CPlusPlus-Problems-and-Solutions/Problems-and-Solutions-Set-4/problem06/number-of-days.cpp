cf1c639bd8bbd66233f30c5a90935be78766015a
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-09-21 22:23:38 +0100

    Number Of Days In A Month

--- CPlusPlus-Problems-and-Solutions/Problems-and-Solutions-Set-4/problem06/number-of-days.cpp
+++ CPlusPlus-Problems-and-Solutions/Problems-and-Solutions-Set-4/problem06/number-of-days.cpp
@@ -1,7 +1,50 @@
-##Problem 05 : Number of Days Hours Minutes Seconds in a month
+#include <iostream>
+using namespace std;
 
-                   Write a program to print number of days,
-               hours,
-               minutes,
-               and seconds in a month.<br><br>
-                   <img src = "problem05.png" alt = "Example output" />
+short ReadNumber(string Msg)
+{
+    int Number;
+
+    cout << Msg;
+    cin >> Number;
+
+    return Number;
+}
+
+bool isLeapYear(short Year)
+{
+    return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Year, short Month)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[] = {31,
+                          28,
+                          31,
+                          30,
+                          31,
+                          30,
+                          31,
+                          31,
+                          30,
+                          31,
+                          30,
+                          31};
+
+    return (Month == 2 && isLeapYear(Year)) ? 29 : NumberOfDays[Month - 1];
+    // return Month == 2 ? (isLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+int main()
+{
+    short Year = ReadNumber("Please enter a year to check: ");
+    short Month = ReadNumber("Please enter a month to check: ");
+
+    cout << "\nNumber of Days in Month[" << Month << "] is " << NumberOfDaysInAMonth(Year, Month) << endl;
+
+    system("pause>0");
+    return 0;
+}
