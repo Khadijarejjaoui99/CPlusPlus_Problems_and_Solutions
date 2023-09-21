d4e5f815be203d8692e6c843247e794a350d3873
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-09-21 22:20:12 +0100

    Update README.md

---
+++ CPlusPlus-Problems-and-Solutions/Problems-and-Solutions-Set-4/problem06/number-of-days-hours-minutes-seconds-in-month-2.cpp
@@ -0,0 +1,7 @@
+##Problem 05 : Number of Days Hours Minutes Seconds in a month
+
+                   Write a program to print number of days,
+               hours,
+               minutes,
+               and seconds in a month.<br><br>
+                   <img src = "problem05.png" alt = "Example output" />
